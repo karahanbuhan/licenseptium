c2da169d2f2181ec202a299be1d5e4724560b98b
Himmet Karahan Buhan <contact@karahanbuhan.com>
2021-09-09 21:17:12 +0300

    Add expiration feature for licenses

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,75 @@
-use actix_web::Result;
-use deadpool_postgres::tokio_postgres::error::Error;
-use deadpool_postgres::Client;
-use serde::Deserialize;
+pub mod config {
+    use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    pub pg: deadpool_postgres::Config,
-}
+    #[derive(Debug, Deserialize)]
+    pub struct Config {
+        pub pg: deadpool_postgres::Config,
+    }
 
-impl Config {
-    pub fn from_env() -> Result<Self, ::config::ConfigError> {
-        let mut cfg = ::config::Config::new();
-        cfg.set_default("pg.dbname", "licenseptium")?;
-        cfg.merge(::config::Environment::new().separator("_"))?;
-        cfg.try_into()
+    impl Config {
+        pub fn from_env() -> Result<Self, ::config::ConfigError> {
+            let mut cfg = ::config::Config::new();
+            cfg.set_default("pg.dbname", "licenseptium")?;
+            cfg.merge(::config::Environment::new().separator("_"))?;
+            cfg.try_into()
+        }
     }
 }
 
-pub async fn create_tables(client: Client) -> Result<(), Error> {
-    client
-        .simple_query(
-            "CREATE TABLE IF NOT EXISTS licenses (
-                id              SERIAL PRIMARY KEY,
-                key             UUID UNIQUE NOT NULL,
-                comment         TEXT NOT NULL
-            )",
-        )
-        .await?;
+pub mod date {
+    use std::error::Error;
 
-    client
-        .simple_query(
-            "CREATE TABLE IF NOT EXISTS validations (
-                ipv4_address    INET NOT NULL,
-                license_id      SERIAL NOT NULL,
-                PRIMARY KEY (ipv4_address, license_id)
-            )",
-        )
-        .await?;
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use tokio_postgres::types::{accepts, FromSql, Type};
 
-    Ok(())
+    // https://github.com/sfackler/rust-postgres/issues/816
+    #[derive(Debug)]
+    pub struct DateTimePlus(pub DateTime<Utc>);
+
+    impl<'a> FromSql<'a> for DateTimePlus {
+        fn from_sql(
+            type_: &Type,
+            raw: &[u8],
+        ) -> Result<DateTimePlus, Box<dyn Error + Sync + Send>> {
+            let naive = match raw {
+                [128, 0, 0, 0, 0, 0, 0, 0] => chrono::naive::MIN_DATETIME,
+                [127, 255, 255, 255, 255, 255, 255, 255] => chrono::naive::MAX_DATETIME,
+                _ => NaiveDateTime::from_sql(type_, raw)?,
+            };
+            Ok(DateTimePlus(DateTime::from_utc(naive, Utc)))
+        }
+
+        accepts!(TIMESTAMPTZ);
+    }
+}
+
+pub mod database {
+    use deadpool_postgres::tokio_postgres::error::Error;
+    use deadpool_postgres::Client;
+
+    pub async fn create_tables(client: Client) -> Result<(), Error> {
+        client
+            .simple_query(
+                r#"
+                CREATE TABLE IF NOT EXISTS licenses (
+                    id              SERIAL PRIMARY KEY,
+                    key             UUID UNIQUE NOT NULL,                
+                    comment         TEXT NOT NULL,
+                    expiry_date     TIMESTAMPTZ NOT NULL
+                )"#,
+            )
+            .await?;
+        client
+            .simple_query(
+                r#"
+                CREATE TABLE IF NOT EXISTS validations (
+                    ipv4_address    INET NOT NULL,
+                    license_id      SERIAL NOT NULL,
+                    PRIMARY KEY (ipv4_address, license_id)
+                )"#,
+            )
+            .await?;
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use std::str::FromStr;
+use std::{cmp::Ordering, str::FromStr};
 
 use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use deadpool_postgres::Pool;
-use licenseptium::{create_tables, Config};
+use licenseptium::{config::Config, database::create_tables, date::DateTimePlus};
 use tokio_postgres::NoTls;
 use uuid::Uuid;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Read environment variables and save to config for postgres
     let cfg = Config::from_env().unwrap();
     let pool = cfg.pg.create_pool(NoTls).unwrap();
+
+    // Create default tables if not exists
     create_tables(pool.get().await.unwrap()).await.unwrap();
 
+    // Start actix server
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
@@ -52,25 +56,33 @@ async fn validate(
     }
     let client = client.unwrap();
 
-    // Return forbidden if license does not exist
-    let row = client
-        .query("SELECT id FROM licenses WHERE key=$1", &[&key])
+    // Get license row by key
+    let rows = client
+        .query("SELECT id, expiry_date FROM licenses WHERE key=$1", &[&key])
         .await
         .unwrap();
-    if row.len() == 0 {
+    // Return forbidden if license does not exist
+    if rows.len() == 0 {
         return HttpResponse::Forbidden();
     }
 
-    let id: i32 = row[0].get(0);
+    let row = &rows[0];
+    let id: i32 = row.get("id");
+    let expiry_date: DateTimePlus = row.get("expiry_date");
+
+    // Return forbidden if license has expired
+    if expiry_date.0.cmp(&chrono::offset::Utc::now()) == Ordering::Less {
+        return HttpResponse::Forbidden();
+    }
 
     // Insert validations into table
     client
-        .execute(
-            "INSERT INTO validations(ipv4_address, license_id) VALUES ($1, $2) ON CONFLICT DO NOTHING",
-            &[&ipv4_addr, &id],
-        )
-        .await
-        .unwrap();
+            .execute(
+                "INSERT INTO validations(ipv4_address, license_id) VALUES ($1, $2) ON CONFLICT DO NOTHING",
+                &[&ipv4_addr, &id],
+            )
+            .await
+            .unwrap();
 
     HttpResponse::Ok()
 }
